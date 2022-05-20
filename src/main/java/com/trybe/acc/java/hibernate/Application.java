e1dc6973d28a1d986fd37c76d362b59f999eb4d1
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-05-20 17:11:27 -0300

    finish

--- src/main/java/com/trybe/acc/java/hibernate/Application.java
+++ src/main/java/com/trybe/acc/java/hibernate/Application.java
@@ -1,5 +1,10 @@
 package com.trybe.acc.java.hibernate;
 
+import com.trybe.acc.java.hibernate.entity.Veiculo;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Application {
 
   /**
@@ -8,7 +13,21 @@ public class Application {
    * @param args argumentos da linha de comando
    */
   public static void main(String[] args) {
+    String path = "com.trybe.acc.java.hibernate.entity.Veiculo";
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory(path);
+    
+    Veiculo veiculo = new Veiculo();
+    veiculo.setAno(2021);
+    veiculo.setModelo("Onix");
+    veiculo.setQuilometragem("60000");
+    
+    EntityManager em = emf.createEntityManager();
 
+    em.getTransaction().begin();
+    em.persist(veiculo);
+    em.getTransaction().commit();
+    
+    em.close(); 
 
   }
 
--- src/main/java/com/trybe/acc/java/hibernate/entity/Veiculo.java
+++ src/main/java/com/trybe/acc/java/hibernate/entity/Veiculo.java
@@ -1,10 +1,57 @@
 package com.trybe.acc.java.hibernate.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Entidade Veículo.
  *
  */
+@Entity
 public class Veiculo {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
 
+  private int ano;
 
+  private String modelo;
+
+  private String quilometragem;
+
+  public int getAno() {
+    return ano;
+  }
+
+  public void setAno(int ano) {
+    this.ano = ano;
+  }
+
+  public String getModelo() {
+    return modelo;
+  }
+
+  public void setModelo(String modelo) {
+    this.modelo = modelo;
+  }
+
+  public String getQuilometragem() {
+    return quilometragem;
+  }
+
+  public void setQuilometragem(String quilometragem) {
+    this.quilometragem = quilometragem;
+  }
+
+  public Long getId() {
+    return this.id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
 }
+
